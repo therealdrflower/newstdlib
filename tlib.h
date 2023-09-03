7793c95acba55c701852df3f20ac24bb7ed513ff
DrFlower <90166542+therealdrflower@users.noreply.github.com>
2023-09-03 08:28:05 -0500

    Init commit

---
+++ tlib.h
@@ -0,0 +1,40 @@
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+
+
+void printstr(std::string text)
+{
+    std::cout << text << std::endl;
+}
+void clear()
+{
+    system("cls");
+    system("clear");
+}
+char getChar()
+{
+    char c;
+    std::cin >> c;
+    return c;
+    
+}
+std::string getWord()
+{
+    std::string input;
+    std::cin >> input;
+    return input;
+}
+std::string charToString(char converted)
+{
+    std::string convert(1,converted);
+    return convert;
+}
+int stringToInt(std::string converted)
+{
+    return stoi(converted);
+}
+int stop(bool type)
+{
+    return type;
+}
